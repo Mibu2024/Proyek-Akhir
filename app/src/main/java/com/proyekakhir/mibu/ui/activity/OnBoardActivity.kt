c558abc7cc43ebe64c7c452cf4cf511a49a65628
shaka pratama ichsan <shakapratama2003@gmail.com>
2024-04-06 12:59:18 +0700

    add new logo

---
+++ app/src/main/java/com/proyekakhir/mibu/ui/activity/OnBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.proyekakhir.mibu.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.proyekakhir.mibu.R
+
+class OnBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+    }
+}
---
+++ app/src/main/java/com/proyekakhir/mibu/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.proyekakhir.mibu.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.proyekakhir.mibu.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
