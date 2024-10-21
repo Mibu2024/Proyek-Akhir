9f6bf8d806c6da1d616f957e117c95baf1b1e431
fadhlansyauqi <adhansyauqi@gmail.com>
2024-10-22 01:04:15 +0700

    change view table on data layanan kb

---
+++ resources/views/data-layanan-kb/components/pagination.blade.php
@@ -0,0 +1,15 @@
+<div class="d-flex justify-content-between align-items-center flex-wrap">
+    <div class="d-flex align-items-center py-3">
+        <span class="text-muted mr-2">Show</span>
+        <form method="GET" action="{{ route('data-layanan-kb.index') }}">
+            <select id="entries" class="form-control form-control-sm font-weight-bold mr-4 border-0 bg-light" style="width: 75px;" name="per_page" onchange="this.form.submit()">
+                @foreach([5, 10, 20, 30] as $perPage)
+                    <option value="{{ $perPage }}" {{ request('per_page') == $perPage ? 'selected' : '' }}>{{ $perPage }}</option>
+                @endforeach
+            </select>
+        </form>
+    </div>
+    <div id="paginationLinks">
+        {{ $data_layanan_kbs->links() }}
+    </div>
+</div>
