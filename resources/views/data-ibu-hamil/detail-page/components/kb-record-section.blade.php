a69fa3a7143311ca3ab66f24b8124444cede150d
shaka pratama ichsan <shakapratama2003@gmail.com>
2025-01-03 13:54:04 +0700

    change kb detail modal title

--- resources/views/data-ibu-hamil/detail-page/components/kb-record-section.blade.php
+++ resources/views/data-ibu-hamil/detail-page/components/kb-record-section.blade.php
@@ -37,7 +37,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="healthRecordModalLabel">Health Record Details</h5>
+        <h5 class="modal-title" id="healthRecordModalLabel">KB Record Details</h5>
       </div>
       <div class="modal-body">
         <div class="container">
